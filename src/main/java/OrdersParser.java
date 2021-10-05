0f37624fca9a27088afac702c19cf60727c3a68a
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-10-05 23:00:27 +0300

    mod validator

--- src/main/java/OrdersParser.java
+++ src/main/java/OrdersParser.java
@@ -8,10 +8,10 @@ public class OrdersParser {
 
     public static void main(String[] args) {
 
-        String[] arr = {".\\src\\main\\resources\\orders44.md",".\\src\\main\\resources\\orders.csv",".\\src\\main\\resources\\orders.jsonl",".\\src\\main\\resources\\orders1.csv"};
+        //String[] arr = {".\\src\\main\\resources\\orders44.md",".\\src\\main\\resources\\orders.csv",".\\src\\main\\resources\\orders.jsonl",".\\src\\main\\resources\\orders1.csv"};
 
-        if (arr.length != 0) {
-            new ArgsValidator().validateArgs(arr);
+        if (args.length != 0) {
+            new ArgsValidator().validateArgs(args);
         } else {
             System.out.println("Incorrect input");
         }
