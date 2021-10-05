1d51389053a63e8097b638a5fdd3a69d38d874ad
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-10-05 22:43:36 +0300

    mod ArgsValidator class

--- src/main/java/ordersparser/ArgsValidator.java
+++ src/main/java/ordersparser/ArgsValidator.java
@@ -6,27 +6,23 @@ public class ArgsValidator {
 
     public void validateArgs(String[] args) {
         for (String path : args) {
-            if (!new File(path).exists()) {
+            File file = new File(path);
+            if (!file.exists()) {
                 System.out.println("File: " + path + " not found");
             } else {
-                if (!getFileExtension(new File(path))) {
-                    System.out.println("Unknown file extension");
+                if (!getFileExtension(file)) {
+                    System.out.println("Unknown file extension: " + file.getName());
                 }
             }
         }
     }
 
     private boolean getFileExtension(File file) {
-        String filename = file.getName();
-        String fileExtension = filename.substring(filename.lastIndexOf(".") + 1);
-        if (filename.lastIndexOf(".") == -1 && filename.lastIndexOf(".") == 0) {
-            return false;
-        }
+        String fileExtension = file.getName().substring(file.getName().lastIndexOf(".") + 1);
         if (fileExtension.equals("csv") || fileExtension.equals("jsonl")) {
             System.out.println(fileExtension);
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 }
