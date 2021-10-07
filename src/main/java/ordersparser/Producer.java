d8c0337d3a6d5c3d5fad8d3a870c450522fa9016
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-10-07 23:22:49 +0300

    added commits

--- src/main/java/ordersparser/Producer.java
+++ src/main/java/ordersparser/Producer.java
@@ -3,6 +3,6 @@ package ordersparser;
 public class Producer implements Runnable {
     @Override
     public void run() {
-        // Считываение данных из файла построчно
+        // Считываение данных из файла построчно и передача в очередь(BlockingQueue<Model> queue)
     }
 }
