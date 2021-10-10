8ac6b5924acff93ab923e741ef33c1cb086fb55e
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-10-10 22:20:52 +0300

    added Model OrderIn

--- src/main/java/ordersparser/model/OrderIn.java
+++ src/main/java/ordersparser/model/OrderIn.java
@@ -2,5 +2,49 @@ package ordersparser.model;
 
 public class OrderIn {
 
+    private String orderId;
+    private String amount;
+    private String currency;
+    private String comment;
+
+    public OrderIn(String orderId, String amount, String currency, String comment) {
+        this.orderId = orderId;
+        this.amount = amount;
+        this.currency = currency;
+        this.comment = comment;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    
 
 }
