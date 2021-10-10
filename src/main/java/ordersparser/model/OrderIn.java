a62e666cc72876778f9910e75408e50a184600a1
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-10-10 22:47:35 +0300

    mod model orderIn

--- src/main/java/ordersparser/model/OrderIn.java
+++ src/main/java/ordersparser/model/OrderIn.java
@@ -1,5 +1,7 @@
 package ordersparser.model;
 
+import java.util.Objects;
+
 public class OrderIn {
 
     private String orderId;
@@ -45,6 +47,36 @@ public class OrderIn {
     public void setComment(String comment) {
         this.comment = comment;
     }
-    
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || obj.getClass() != getClass()) return false;
+        OrderIn orderIn = (OrderIn) obj;
+        return Objects.equals(orderId, ((OrderIn) obj).orderId) && Objects.equals(amount, ((OrderIn) obj).amount) &&
+                Objects.equals(currency, ((OrderIn) obj).currency) &&
+                comment.equals(((OrderIn) obj).comment);
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + (orderId == null ? 0 : orderId.hashCode());
+        result = prime * result + (amount == null ? 0 : amount.hashCode());
+        result = prime * result + (currency == null ? 0 : currency.hashCode());
+        result = prime * result + (comment == null ? 0 : comment.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("OrderIn: {");
+        stringBuilder.append("orderId = ").append(orderId);
+        stringBuilder.append("amount = ").append(amount);
+        stringBuilder.append("currency = ").append(currency);
+        stringBuilder.append("comment = ").append(comment);
+        stringBuilder.append("}");
+        return stringBuilder.toString();
+    }
 }
