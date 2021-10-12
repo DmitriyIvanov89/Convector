3028db3d177b4a6e1ea7bac9b69b9743d4772b0f
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-10-12 22:17:39 +0300

    mod producers

--- src/main/java/ordersparser/producer/CsvProducer.java
+++ src/main/java/ordersparser/producer/CsvProducer.java
@@ -1,10 +1,20 @@
 package ordersparser.producer;
 
+import java.util.concurrent.BlockingQueue;
+
 public class CsvProducer implements Runnable {
+
+    private String path;
+    private BlockingQueue<Object> queue;
+    private ProducerType type;
 
     @Override
     public void run() {
         // Считываение данных из csv файла построчно и передача в очередь(BlockingQueue<Model> queue)
 
+    }
+
+    public ProducerType getType() {
+        return ProducerType.CSV_PRODUCER;
     }
 }
