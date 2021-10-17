9333fa75a9af6aba868e7854a31d993aaa6aa1f6
DmitriyIvanov89 <cbr1000rr8@mail.ru>
2021-10-17 21:25:29 +0300

    mod consumer

--- src/main/java/ordersparser/consumer/Consumer.java
+++ src/main/java/ordersparser/consumer/Consumer.java
@@ -2,6 +2,8 @@ package ordersparser.consumer;
 
 public class Consumer implements Runnable {
 
+
+
     @Override
     public void run() {
         // Обработка сообщения из очереди (BlockingQueue<Model> queue), конвектирование в формат json
