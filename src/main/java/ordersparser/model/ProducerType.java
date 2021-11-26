fb552fa3d44861696ae4ef6589e34b6c62d1736b
[DmitriyIvanov89] <[cbr1000rr8@mail.ru]>
2021-11-26 17:34:10 +0300

    mod project

--- src/main/java/ordersparser/model/ProducerType.java
+++ src/main/java/ordersparser/model/ProducerType.java
@@ -1,14 +1,10 @@
 package ordersparser.model;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum ProducerType {
     CSV("CSV"),
     JSONL("JSONL");
 
     private final String type;
-    private static final Map<String, ProducerType> ENUM_MAP = new HashMap<>();
 
     ProducerType(String type) {
         this.type = type;
